3cecf92d3bd548a6414a3e580437e8f6a1038430
PriyankaKamble <priyankakamble18042003@gmail.com>
2025-10-18 17:44:48 +0530

    hashsetapp

---
+++ HashSetApp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class HashSetApp
+{
+	public static void main(String x[])
+	{
+	HashSet<Integer> h=new HashSet<>();
+	h.add(10);
+	h.add(20);
+	h.add(30);
+	h.add(40);
+	h.add(50);
+	h.add(60);
+	h.add(70);
+	h.add(80);
+	h.add(90);
+	h.add(100);
+
+	System.out.println("Display HashSet Values:"+h);
+	}
+}
